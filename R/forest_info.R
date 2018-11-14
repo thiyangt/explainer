681a12f62f3f7da2cc4ed707a7adaa04a024bd20
thiyangt <thiyanga.talagala@monash.edu>
2018-11-14 15:25:53 +1100

    add forest_info function

---
+++ R/forest_info.R
@@ -0,0 +1,21 @@
+#' Function to extract tree information in the forest
+#'
+#' Function to extract tree information in the forest
+#' @param forest name of the randomForest model
+#' @param ntree number of trees in the forest
+#' @return retun a list of variable information corresponds to each tree
+#' @author Thiyanga Talagala
+#' @export
+forest_info <- function(forest, ntree){
+index <- 1:ntree
+lapply(index, function(temp){
+  info <- randomForest::getTree(forest, temp, labelVar=TRUE)[,"split var"]
+  info <-  info[!is.na(info)]
+  })
+}
+#' @example
+#' library(randomForest)
+#' set.seed(2018)
+#' forest <- randomForest(Species ~ ., data = iris, ntree=2)
+#' forest_info(forest, 2)
+
